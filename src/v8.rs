ec0ecfbc4e6c2afbfc2e668987d1d72a92496f48
迷渡 <justjavac@gmail.com>
2021-01-22 17:19:11 +0800

    remove comment
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- src/v8.rs
+++ src/v8.rs
@@ -1,19 +1,17 @@
 use rusty_v8 as v8;
 
 fn main() {
-  // 初始化 V8
+  // init V8
   let platform = v8::new_default_platform().unwrap();
   v8::V8::initialize_platform(platform);
   v8::V8::initialize();
 
-  // 新疆 Isolate
   let isolate = &mut v8::Isolate::new(Default::default());
 
   let scope = &mut v8::HandleScope::new(isolate);
   let context = v8::Context::new(scope);
   let scope = &mut v8::ContextScope::new(scope, context);
 
-  // 生成 JS 字符串 hello world
   let code = v8::String::new(scope, "'Hello' + ' World!'").unwrap();
   println!("javascript code: {}", code.to_rust_string_lossy(scope));
 
